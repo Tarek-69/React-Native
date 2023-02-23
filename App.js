7b89ef28fbfb51bd4662fa4fdb32e934e6236f64
Tarek Taguine <t.taguine@it-students.fr>
2023-02-23 11:55:16 +0100

    Mise à jours du design

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   TextInput,
   TextInputComponent,
   View,
+  FlatList,
 } from "react-native";
 
 export default function App() {
@@ -26,18 +27,36 @@ export default function App() {
     "Organiser un meetup autour de la tech",
     "Faire un triathlon",
   ];
+
   return (
     <View style={styles.container}>
       <SafeAreaView>
-        <Text style={styles.baseText}>Mon texte est rouge</Text>
         <Text style={styles.baseText}>App.js</Text>
-        <View style={styles.row}>
+
+        <View style={styles.inputContainer}>
           <TextInput
             style={styles.input}
             placeholder="Entrez votre texte ici"
           />
+
           <Button title="Ajouter un item " onPress={() => sampleGoals} />
         </View>
+
+        <FlatList
+          data={[
+            { key: "Faire les courses" },
+            { key: "Aller à la salle de sport 3 fois par semaine" },
+            { key: "Monter à plus de 5000m d altitude" },
+            { key: "Acheter mon premier appartement" },
+            { key: "Perdre 5 kgs" },
+            { key: "Gagner en productivité" },
+            { key: "Apprendre un nouveau langage" },
+            { key: "Faire une mission en freelance" },
+            { key: "Organiser un meetup autour de la tech" },
+            { key: "Faire un triathlon" },
+          ]}
+          renderItem={({ item }) => <Text style={styles.list}>{item.key}</Text>}
+        />
       </SafeAreaView>
     </View>
   );
@@ -48,22 +67,31 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 40,
   },
   baseText: {
     fontWeight: "bold",
     color: "red",
     textAlign: "center",
+    margin: 23,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 40,
   },
   input: {
     height: 40,
     borderWidth: 1,
     padding: 10,
     width: "50%",
-    margin: 10,
+    marginRight: 0,
   },
-  row: {
-    flexDirection: "row",
-    alignItems: "center",
-    margin: 12,
+
+  list: {
+    marginLeft: 0,
+    fontSize: 12,
+    marginVertical: 10,
   },
 });
