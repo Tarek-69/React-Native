57cd191c07898d2b5afc42e5c99f5ff07848fe16
Tarek Taguine <t.taguine@it-students.fr>
2023-02-24 10:09:13 +0100

    Add Modal and function delete item

--- Components/Todo.js
+++ Components/Todo.js
@@ -1,52 +1,47 @@
-import { View, Text, Button, StyleSheet, Pressable, Modal, Alert } from "react-native";
-
-
-
-const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-
-<View style={styles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}>
-        <Text style={styles.textStyle}>Show Modal</Text>
-      </Pressable>
-    </View>
-}
-
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, Modal } from "react-native";
 
 function Todo(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleDelete = () => {
+    props.deleteTodo(props.index);
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.listItemContainer}>
       <Text style={styles.listItem}>{props.todo}</Text>
       <Button
         title="Supprimer la tâche"
-        onPress={() => props.deleteTodo(props.index)}
+        onPress={() => setModalVisible(true)}
       />
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Supprimer la tâche ?</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Button
+                style={{ ...styles.button, ...styles.buttonClose }}
+                title="Annuler"
+                onPress={() => setModalVisible(false)}
+              />
+              <Button
+                style={{ ...styles.button, ...styles.buttonOpen }}
+                title="Confirmer"
+                onPress={handleDelete}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
-
-export default Todo;
 
 const styles = StyleSheet.create({
   listItemContainer: {
@@ -65,43 +60,27 @@ const styles = StyleSheet.create({
   },
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 22,
   },
   modalView: {
     margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
+    backgroundColor: "rgba(22, 4, 115, 0.8)",
+    borderRadius: 10,
     padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+    alignItems: "center",
   },
   button: {
     borderRadius: 20,
     padding: 10,
     elevation: 2,
   },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+
   modalText: {
     marginBottom: 15,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
+
+export default Todo;
